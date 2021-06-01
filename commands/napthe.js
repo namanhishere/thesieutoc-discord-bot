c2fc3af26ce4400f2c8333eb70901f6393b6fe66
namanhishere <namanhishere0208@gmail.com>
2021-06-01 13:13:33 +0700

    fix napthe

--- commands/napthe.js
+++ commands/napthe.js
@@ -12,7 +12,7 @@ module.exports.run = async (client, message, args) => {
             message.reply("Bạn chưa yêu cài đặt hệ thống, vui lòng liên hệ quản trị viên hoặc sử dụng lệnh ```!setup```. Xin cảm ơn")
         } else {
             if(data[0].isOpen === "no") return message.reply("Server không nhận thẻ vào thời điểm hiện tại. Vui lòng liên hệ admin server")
-            if (!args[0] || !args[1] || !args[2] || !args[3]) return message.reply("Vui lòng nhập đầy đủ các thông tin. Câu lệnh sử dụng là ```" + data[0].prefix + "napthe [Tên nhà mang] [Mệnh giá.] [Số Seri] [Số thẻ]```")
+            if (!args[0] || !args[1] || !args[2] || !args[3]) return message.reply("Vui lòng nhập đầy đủ các thông tin. Câu lệnh sử dụng là ```" + data[0].prefix + "napthe [Tên nhà mang] [Mệnh giá.] [Số Thẻ] [Số Seri]```")
             if ((args[0] == "Viettel" || args[0] == "Mobifone" || args[0] === "Vinaphone" || args[0] === "Zing" || args[0] === "Vietnamobile" || args[0] === "Vcoin" || args[0] === "Gate") === false) return message.reply("Thẻ của bạn có thể nhập sai hoặc không được hỗ trợ, vui lòng kiểm tra lại. Chú ý, viết đúng cả hoa lẫn viết thường: ```- Viettel\n- Mobifone\n- Vinaphone\n- Zing\n- Vietnamobile\n- Vcoin\n- Gate```")
             if ((isNaN(parseFloat(args[1])) || args[1] === '10000' || args[1] === '20000' || args[1] === '30000' || args[1] === '50000' || args[1] === '100000' || args[1] === '200000'||args[1] === '300000'||args[1] === '500000') === false) return message.reply("Mệnh giá nạp tiền không tồn tại")
             if(isNaN(parseFloat(args[2]))||isNaN(parseFloat(args[3]))) return message.reply("Thẻ nạp phải ở dạng số, vui lòng kiểm tra lại cú pháp```" + data[0].prefix + "napthe [Tên nhà mang] [Mệnh giá.] [Số Thẻ] [Số Seri]```")
